5c2d60bf05434fe57ab74af95e83b8872c161196
Chris Greene <archgoon@gmail.com>
2017-02-05 19:01:15 -0800

    Flesh out how inotify callback will work
    Beginning to look like an actual program!
    
    So what we're doing is using an MVar to persist state between event
    handler calls. This allows us to go from
    
      Event -> IO()
    
    To
    
      State -> Event -> State
    
    As an added bonus, the main thread can see what files were opened
    before shutting down.

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,32 @@
+import Control.Concurrent.MVar
+import qualified Data.Map.Strict as Map
+
 import System.Directory
 import System.IO
 
 import System.INotify
 
-eventHandler :: INotify -> [IO WatchDescriptor] -> Event -> IO()
-eventHandler inotify e@(Created isDirectory filePath) = print e
-eventHandler inotify e = return ()
+eventHandler :: MVar (Map.Map String String) -> INotify -> Event -> IO()
+eventHandler mvar inotify e@(Created isDirectory filePath) = do
+  state <- takeMVar mvar
+  print e
+  let newState = Map.insert filePath "fileHandleHere" state
+  putMVar mvar newState
+eventHandler mvar inotify e = return ()
 
 main = do
   inotify <- initINotify
   print inotify
   let watchDirectory = "/tmp/"
+  mvar <- newMVar Map.empty
   wd <- addWatch
           inotify
           [Open, Close, Access, Modify, Move, Create]
           watchDirectory
-          (eventHandler inotify wds)
+          (eventHandler mvar inotify)
   print wd
   putStrLn "Listens"
   getLine
+  files <- takeMVar mvar
+  putStrLn $ show $ Map.keys files
   removeWatch wd
