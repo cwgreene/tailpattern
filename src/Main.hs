601e084f1fd39c7a0e8198fd733e5569e93348e2
Chris Greene <archgoon@gmail.com>
2017-02-06 23:13:21 -0800

    Command line arguments.
    Don't like how this is working; but it works!

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,13 @@ import qualified System.FilePath.Glob as Glob
 import qualified Data.Map.Strict as Map
 
 import System.Directory
+import System.Environment
 import System.IO
 
 import System.INotify
 
 type State = Map.Map String String
+type EventTypeHandler = State -> Event -> State
 
 patternFilter :: Glob.Pattern -> String -> State -> (State -> String -> State) -> State
 patternFilter pattern filePath s handler =
@@ -16,34 +18,41 @@ patternFilter pattern filePath s handler =
   else
     s
 
-pat = Glob.compile "doom*"
-
-handler :: State -> Event -> State
-handler s e@(Created isDirectory filePath) =
-  patternFilter pat filePath s $ \s fp -> Map.insert fp "fileHandleHere" s
-handler s e@(Modified isDirectory maybeFilePath) =
+handler :: Glob.Pattern -> State -> Event -> State
+handler pattern s e@(Created isDirectory filePath) =
+  patternFilter pattern filePath s $ \s fp -> Map.insert fp "fileHandleHere" s
+handler pattern s e@(Modified isDirectory maybeFilePath) =
   case maybeFilePath of
     Nothing -> s
-    Just filePath -> patternFilter pat filePath s $ \s fp -> Map.insert fp "modified" s
-handler s e = s
+    Just filePath -> patternFilter pattern filePath s $ \s fp -> Map.insert fp "modified" s
+handler p s e = s
 
-eventHandler :: MVar State -> INotify -> Event -> IO()
-eventHandler mvar inotify e = do
+eventHandler :: MVar State -> INotify -> EventTypeHandler -> Event -> IO()
+eventHandler mvar inotify handler e = do
   state <- takeMVar mvar
   print e
   let newState = handler state e
   putMVar mvar newState
 
 main = do
+  args <- getArgs
+  if length args /= 2 then
+    -- TODO: Make this not silly.
+    fail "Invalid Number of Supplied Arguments"
+  else
+    -- Not what one would expect in something like Python.
+    return ()
+  let watchDirectory = args !! 0
+  let patternGlob = args !! 1
+  let pattern = Glob.compile patternGlob
   inotify <- initINotify
   print inotify
-  let watchDirectory = "/tmp/"
   mvar <- newMVar Map.empty
   wd <- addWatch
           inotify
           [Open, Close, Access, Modify, Move, Create]
           watchDirectory
-          (eventHandler mvar inotify)
+          (eventHandler mvar inotify (handler pattern))
   print wd
   putStrLn "Listens"
   getLine
