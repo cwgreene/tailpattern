f56b1b1be29078bab467666ab03c604c93912700
Chris Greene <archgoon@gmail.com>
2017-02-15 11:52:21 -0800

    Allowing IO actions in handler, updated types.
    An alternative approach is to try to isolate the state transforms from
    IO actions, and then always execute the IO actions after updating the
    state first. However, the appropriate state updates rely on IO actions
    (like the length of the file). Not clear if that's a good reason to
    return IO State, or if it's actually call for a loop "Update
    State, if yielded IO action, execute, go back, stop if no change"

--- src/Main.hs
+++ src/Main.hs
@@ -9,30 +9,43 @@ import System.IO
 import System.INotify
 
 type State = Map.Map String String
-type EventTypeHandler = State -> Event -> State
-type EventFileHandler = State -> String -> State
+type EventTypeHandler = State -> Event -> IO State
+type EventFileHandler = State -> String -> IO State
 
-patternFilter :: Glob.Pattern -> String -> State -> (EventFileHandler) -> State
+patternFilter :: Glob.Pattern -> String -> State -> (EventFileHandler) -> IO State
 patternFilter pattern filePath s handler =
   if (Glob.match pattern filePath) then
     handler s filePath
   else
-    s
+    return s
+
+handleNewFile :: EventFileHandler
+handleNewFile oldState path = do
+    let newState = Map.insert path "fileHandleHere" oldState
+    -- cat file here, and update length of file
+    -- need to change state to Map.Map String (FileHandle length)
+    return newState
+
+handleUpdatedFile :: EventFileHandler
+handleUpdatedFile oldState path = do
+    let newState = oldState
+    -- find position from the old state seek to it, and resume reading to EOF
+    -- update state to new length
+    return newState
 
 eventHandler :: Glob.Pattern -> EventTypeHandler
 eventHandler pattern s e@(Created isDirectory filePath) =
-  patternFilter pattern filePath s $ \s fp -> Map.insert fp "fileHandleHere" s
+  patternFilter pattern filePath s $ \s fp -> return $ Map.insert fp "fileHandleHere" s
 eventHandler pattern s e@(Modified isDirectory maybeFilePath) =
   case maybeFilePath of
-    Nothing -> s
-    Just filePath -> patternFilter pattern filePath s $ \s fp -> Map.insert fp "modified" s
-eventHandler p s e = s
+    Nothing -> return s
+    Just filePath -> patternFilter pattern filePath s $ \s fp -> return $ Map.insert fp "modified" s
+eventHandler p s e = return s
 
 inotifyCallback :: MVar State -> INotify -> EventTypeHandler -> Event -> IO()
 inotifyCallback mvar inotify eventTypeHandler e = do
-  state <- takeMVar mvar
-  print e
-  let newState = eventTypeHandler state e
+  oldState <- takeMVar mvar
+  newState <- eventTypeHandler oldState e
   putMVar mvar newState
 
 main = do
